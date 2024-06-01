b1a91918652749f8540dc3b9f17db59bb97345cb
Muhammad Abdullah <iamagoodboi4@gmail.com>
2024-06-01 13:48:00 +0500

    made changes in the input fields

--- src/components/Form1.jsx
+++ src/components/Form1.jsx
@@ -1,8 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import towelImg from "../assets/images/background.jpg";
 import logo from "../assets/images/logomain.png";
-import { Inputfields } from "./index.js";
+
 const Form1 = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [department, setDepartment] = useState("");
+  const [designation, setDesignation] = useState("");
+  const [section, setSection] = useState("");
+  const [contact, setContact] = useState("");
+
+  // Debugging: Check the initial state
+  // console.log({ name, email, department, designation, section, contact });
   return (
     <div className="relative h-screen w-screen">
       <img
@@ -18,17 +27,143 @@ const Form1 = () => {
         <h2 className="font-Montserrat text-[36px] font-bold mb-4">
           Issue generator
         </h2>
-        <div class="flex items-center justify-between">
-          <div class="font-Montserrat text-[20px]">CAPA number</div>
-          <div class="font-Montserrat text-[20px]">#1001</div>
+        <div className="flex items-center justify-between">
+          <div className="font-Montserrat text-[20px]">CAPA number</div>
+          <div className="font-Montserrat text-[20px]">#1001</div>
         </div>
+        <div>
+          {/* Name */}
+          <div className="relative mb-4 mt-4">
+            <input
+              id="Name"
+              type="text"
+              value={name}
+              onChange={(event) => {
+                console.log("Name:", event.target.value); // Debugging
+                setName(event.target.value);
+              }}
+              className="font-Montserrat block w-full px-2 py-2 text-lg bg-transparent border-b border-gray-400 appearance-none focus:outline-none focus:ring-0 focus:border-gray-600 peer"
+              placeholder=" "
+            />
+            <label
+              htmlFor="Name"
+              className="absolute left-0 top-2.5 text-gray-900 text-lg duration-300 transform -translate-y-6 scale-75 origin-0 peer-placeholder-shown:scale-100 
+            peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 peer-focus:text-gray-600 font-Montserrat"
+            >
+              Name
+            </label>
+          </div>
 
-        <Inputfields label="Name" id="Name" />
-        <Inputfields label="Department" id="Department" />
-        <Inputfields label="Designation" id="Designation" />
-        <Inputfields label="Section" id="Section" />
-        <Inputfields label="Email" id="Email" />
-        <Inputfields label="Contact" id="Contact" />
+          {/* Email */}
+          <div className="relative mb-4">
+            <input
+              id="Email"
+              type="text"
+              value={email}
+              onChange={(event) => {
+                console.log("Email:", event.target.value); // Debugging
+                setEmail(event.target.value);
+              }}
+              className="font-Montserrat block w-full px-2 py-2 text-lg bg-transparent border-b border-gray-400 appearance-none focus:outline-none focus:ring-0 focus:border-gray-600 peer"
+              placeholder=" "
+            />
+            <label
+              htmlFor="Email"
+              className="absolute left-0 top-2.5 text-gray-900 text-lg duration-300 transform -translate-y-6 scale-75 origin-0 peer-placeholder-shown:scale-100 
+            peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 peer-focus:text-gray-600 font-Montserrat"
+            >
+              Email
+            </label>
+          </div>
+
+          {/* Department */}
+          <div className="relative mb-4">
+            <input
+              id="Department"
+              type="text"
+              value={department}
+              onChange={(event) => {
+                console.log("Department:", event.target.value); // Debugging
+                setDepartment(event.target.value);
+              }}
+              className="font-Montserrat block w-full px-2 py-2 text-lg bg-transparent border-b border-gray-400 appearance-none focus:outline-none focus:ring-0 focus:border-gray-600 peer"
+              placeholder=" "
+            />
+            <label
+              htmlFor="Department"
+              className="absolute left-0 top-2.5 text-gray-900 text-lg duration-300 transform -translate-y-6 scale-75 origin-0 peer-placeholder-shown:scale-100 
+            peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 peer-focus:text-gray-600 font-Montserrat"
+            >
+              Department
+            </label>
+          </div>
+
+          {/* Designation */}
+          <div className="relative mb-4">
+            <input
+              id="Designation"
+              type="text"
+              value={designation}
+              onChange={(event) => {
+                console.log("Designation:", event.target.value); // Debugging
+                setDesignation(event.target.value);
+              }}
+              className="font-Montserrat block w-full px-2 py-2 text-lg bg-transparent border-b border-gray-400 appearance-none focus:outline-none focus:ring-0 focus:border-gray-600 peer"
+              placeholder=" "
+            />
+            <label
+              htmlFor="Designation"
+              className="absolute left-0 top-2.5 text-gray-900 text-lg duration-300 transform -translate-y-6 scale-75 origin-0 peer-placeholder-shown:scale-100 
+            peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 peer-focus:text-gray-600 font-Montserrat"
+            >
+              Designation
+            </label>
+          </div>
+
+          {/* Section */}
+          <div className="relative mb-4">
+            <input
+              id="Section"
+              type="text"
+              value={section}
+              onChange={(event) => {
+                console.log("Section:", event.target.value); // Debugging
+                setSection(event.target.value);
+              }}
+              className="font-Montserrat block w-full px-2 py-2 text-lg bg-transparent border-b border-gray-400 appearance-none focus:outline-none focus:ring-0 focus:border-gray-600 peer"
+              placeholder=" "
+            />
+            <label
+              htmlFor="Section"
+              className="absolute left-0 top-2.5 text-gray-900 text-lg duration-300 transform -translate-y-6 scale-75 origin-0 peer-placeholder-shown:scale-100 
+            peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 peer-focus:text-gray-600 font-Montserrat"
+            >
+              Section
+            </label>
+          </div>
+
+          {/* Contact */}
+          <div className="relative mb-4">
+            <input
+              id="Contact"
+              type="text"
+              value={contact}
+              onChange={(event) => {
+                console.log("Contact:", event.target.value); // Debugging
+                setContact(event.target.value);
+              }}
+              className="font-Montserrat block w-full px-2 py-2 text-lg bg-transparent border-b border-gray-400 appearance-none focus:outline-none focus:ring-0 focus:border-gray-600 peer"
+              placeholder=" "
+            />
+            <label
+              htmlFor="Contact"
+              className="absolute left-0 top-2.5 text-gray-900 text-lg duration-300 transform -translate-y-6 scale-75 origin-0 peer-placeholder-shown:scale-100 
+            peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 peer-focus:text-gray-600 font-Montserrat"
+            >
+              Contact
+            </label>
+          </div>
+        </div>
 
         <button className="font-Montserrat bg-primary-button text-white px-4 py-2 rounded hover:bg-primary-button-hover w-full">
           Next
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,35 +1,148 @@
-import React from 'react'
+import React, { useState } from "react";
 import towelImg from "../assets/images/background.jpg";
 import logo from "../assets/images/logomain.png";
-import Inputfields from './index.js';
 
 const Signup = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confrimpassword, setConfirmpassword] = useState("");
+
+  // console.log({ name, email, password, confrimpassword });
+  
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const toggleConfirmPasswordVisibility = () => {
+    setShowConfirmPassword(!showConfirmPassword);
+  };
   return (
     <div className="relative h-screen w-screen">
-    <img
-      src={towelImg}
-      alt="Background"
-      className="object-cover h-full w-full"
-    />
-    <div className="absolute top-0 left-0 h-full w-full bg-black opacity-50"></div>
-    <div className="hidden lg:block absolute top-[50%] left-[25%] transform -translate-x-[50%] -translate-y-[50%] text-center">
-      <img src={logo} alt="LargeLogomain" />
-    </div>
-    <div className="absolute top-[55%] lg:top-[55%] left-[50%] lg:left-[75%] transform -translate-x-[50%] -translate-y-[50%] lg:translate-y-[-50%] w-full lg:w-[510px] h-full lg:h-[750px] bg-white p-4 rounded-lg shadow-lg mt-20 lg:mt-0">
-      <h2 className="font-Montserrat text-[36px] font-bold mb-4">
-      Sign Up
-      </h2>
+      <img
+        src={towelImg}
+        alt="Background"
+        className="object-cover h-full w-full"
+      />
+      <div className="absolute top-0 left-0 h-full w-full bg-black opacity-50"></div>
+      <div className="hidden lg:block absolute top-[50%] left-[25%] transform -translate-x-[50%] -translate-y-[50%] text-center">
+        <img src={logo} alt="LargeLogomain" />
+      </div>
 
-      <Inputfields label="Name" id="Name" />
-      <Inputfields label="Email" id="Email" />
-      <Inputfields label="Password" id="Password" type='password' />
-      <Inputfields label="Confirm Password" id="ConfirmPassword"  type='password'/>
-      <button className="font-Montserrat bg-primary-button text-white px-4 py-2 rounded hover:bg-primary-button-hover w-full">
-        Next
-      </button>
-    </div>
-  </div>
-  )
-}
+      {/* inputs div */}
+      <div className="absolute top-[55%] lg:top-[55%] left-[50%] lg:left-[75%] transform -translate-x-[50%] -translate-y-[50%] lg:translate-y-[-50%] w-full lg:w-[510px] h-full lg:h-[750px] bg-white p-4 rounded-lg shadow-lg mt-20 lg:mt-0">
+        <h2 className="font-Montserrat text-[36px] font-bold mb-4">Sign Up</h2>
 
-export default Signup
+        <div className="relative mb-4">
+          <input
+            id="Name"
+            type="text"
+            value={name}
+            onChange={(event) => {
+              console.log("Name:", event.target.value); // Debugging
+              setName(event.target.value);
+            }}
+            className="font-Montserrat block w-full px-2 py-2 text-lg bg-transparent border-b border-gray-400 appearance-none focus:outline-none focus:ring-0 focus:border-gray-600 peer"
+            placeholder=" "
+          />
+          <label
+            htmlFor="Name"
+            className="absolute left-0 top-2.5 text-gray-900 text-lg duration-300 transform -translate-y-6 scale-75 origin-0 peer-placeholder-shown:scale-100 
+          peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 peer-focus:text-gray-600 font-Montserrat"
+          >
+            Name
+          </label>
+        </div>
+
+        {/* email */}
+        <div className="relative mb-4">
+          <input
+            id="Email"
+            type="email"
+            value={email}
+            onChange={(event) => {
+              console.log("Email:", event.target.value); // Debugging
+              setEmail(event.target.value);
+            }}
+            className="font-Montserrat block w-full px-2 py-2 text-lg bg-transparent border-b border-gray-400 appearance-none focus:outline-none focus:ring-0 focus:border-gray-600 peer"
+            placeholder=" "
+          />
+          <label
+            htmlFor="Email"
+            className="absolute left-0 top-2.5 text-gray-900 text-lg duration-300 transform -translate-y-6 scale-75 origin-0 peer-placeholder-shown:scale-100 
+          peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 peer-focus:text-gray-600 font-Montserrat"
+          >
+            Email
+          </label>
+        </div>
+
+        <div>
+          {/* Password */}
+          <div className="relative mb-4">
+            <input
+              id="Password"
+              value={password}
+              onChange={(event) => {
+                console.log("password:", event.target.value); // Debugging
+                setPassword(event.target.value);
+              }}
+              type={showPassword ? "text" : "password"}
+              className="font-Montserrat block w-full px-2 py-2 text-lg bg-transparent border-b border-gray-400 appearance-none focus:outline-none focus:ring-0 focus:border-gray-600 peer"
+              placeholder=" "
+            />
+            <label
+              htmlFor="Password"
+              className="absolute left-0 top-2.5 text-gray-900 text-lg duration-300 transform -translate-y-6 scale-75 origin-0 peer-placeholder-shown:scale-100 
+          peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 peer-focus:text-gray-600 font-Montserrat"
+            >
+              Password
+            </label>
+            <button
+              type="button"
+              onClick={togglePasswordVisibility}
+              className="absolute right-2 top-2.5 text-lg text-gray-600 focus:outline-none"
+            >
+              {showPassword ? "Hide" : "Show"}
+            </button>
+          </div>
+
+          {/* Confirm Password */}
+          <div className="relative mb-4">
+            <input
+              id="Confirm Password"
+              value={confrimpassword}
+              onChange={(event) => {
+                console.log("confrim password:", event.target.value); // Debugging
+                setConfirmpassword(event.target.value);
+              }}
+              type={showConfirmPassword ? "text" : "password"}
+              className="font-Montserrat block w-full px-2 py-2 text-lg bg-transparent border-b border-gray-400 appearance-none focus:outline-none focus:ring-0 focus:border-gray-600 peer"
+              placeholder=" "
+            />
+            <label
+              htmlFor="Confirm Password"
+              className="absolute left-0 top-2.5 text-gray-900 text-lg duration-300 transform -translate-y-6 scale-75 origin-0 peer-placeholder-shown:scale-100 
+          peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 peer-focus:text-gray-600 font-Montserrat"
+            >
+              Confirm Password
+            </label>
+            <button
+              type="button"
+              onClick={toggleConfirmPasswordVisibility}
+              className="absolute right-2 top-2.5 text-lg text-gray-600 focus:outline-none"
+            >
+              {showConfirmPassword ? "Hide" : "Show"}
+            </button>
+          </div>
+        </div>
+        <button className="font-Montserrat bg-primary-button text-white px-4 py-2 rounded hover:bg-primary-button-hover w-full">
+          Next
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Signup;
